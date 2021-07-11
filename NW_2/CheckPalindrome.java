3e54414c840256b3c6e2ae2761f0587734dc9c98
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-07-11 17:39:20 +0300

    Update CheckPalindrome.java

--- NW_2/CheckPalindrome.java
+++ NW_2/CheckPalindrome.java
@@ -1,18 +1,17 @@
 package palindrome;
 
 public class CheckPalindrome implements Palindrome{
-    private int number1;
     private int reverse;
 
     @Override
     public boolean isPalindrome(int x) {
-        number1=Math.abs(x);
+        int modulNumber=Math.abs(x);
         while (x != 0) {
                 int remember = x % 10;
                 reverse = reverse * 10 + remember;
                 x = x / 10;
         }
-        return number1 == Math.abs(reverse);
+        return modulNumber == Math.abs(reverse);
 
     }
 }
