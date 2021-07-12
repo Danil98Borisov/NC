c4af8d293a522225d51a563b14b33a3d48a6774a
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-07-12 07:10:28 +0300

    Update Main.java

--- NW_2/Main.java
+++ NW_2/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main (String[] args){
         checkPalindrome = new CheckPalindrome();
 
-        System.out.format("Is it a palindrome? - %s \n", checkPalindrome.isPalindrome(-10));
-
+        System.out.format("Is it a palindrome? - %s \n Is it a palindrome? - %s ", checkPalindrome.isPalindrome(121),checkPalindrome.isPalindrome(454));
     }
 }
