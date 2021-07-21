8366a591c2aa42a10d0675a17a87961d2f167d9c
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-07-21 13:29:33 +0300

    Update SumOfTwoMatricesTest.java

--- HW_3/SumOfTwoMatricesTest.java
+++ HW_3/SumOfTwoMatricesTest.java
@@ -10,16 +10,20 @@ class SumOfTwoMatricesTest {
     @Test
     void getElementNull() {
         int[][] b = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-        int[][] a = {{13,14,15},{16,17,18},{19,20,21},{22,23,24}};
-        int currentResult = new SumOfTwoMatrices(a,b).getElement(0,0);
-        Assert.assertEquals(14,currentResult);
+        int[][] a = {{13,14,15}, {16,17,18}, {19,20,21}, {22,23,24}};
+        MatrixImpl impl1 = new MatrixImpl(a);
+        MatrixImpl impl2 = new MatrixImpl(b);
+        int currentResult = new SumOfTwoMatrices(impl1, impl2).getElement(2,1);
+        Assert.assertEquals(28,currentResult);
     }
 
     @Test
     void getElementTest() {
         int[][] b = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
         int[][] a = {{13,14,15},{16,17,18},{19,20,21},{22,23,24}};
-        int currentResult = new SumOfTwoMatrices(a,b).getElement(3,2);
+        MatrixImpl impl1 = new MatrixImpl(a);
+        MatrixImpl impl2 = new MatrixImpl(b);
+        int currentResult = new SumOfTwoMatrices(impl1,impl2).getElement(3,2);
         Assert.assertEquals(36,currentResult);
     }
 }
