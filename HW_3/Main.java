47d89b26f1d4307696bc223877c22cd4d994bc98
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-07-21 13:27:50 +0300

    Update Main.java

--- HW_3/Main.java
+++ HW_3/Main.java
@@ -1,60 +1,55 @@
 package decorator;
 
-public class Main {
-    static int n;
-    static int m;
+public class Main{
 
-    public static void main(String[] args) {
-        n = 3;
-        m = 7;
-        int[][] a = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                a[i][j] = n * i + j;
+    public static int[][] createMatrix1(int i, int j){
+        int[][] matrix = new int[i][j];
+        for (i = 0; i < matrix.length; i++) {
+            for (j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 3*i + j;
             }
         }
-        System.out.println("Начальная матрица a:");
-        System.out.println("------");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.printf("%4d", a[i][j]);
-            }
-            System.out.println();
-        }
-        int[][] b = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                b[i][j] = i + m * j;
+        return matrix;
+    }
+    public static int[][] createMatrix2(int i, int j){
+        int[][] matrix = new int[i][j];
+        for (i = 0; i < matrix.length; i++) {
+            for (j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 2*i + j;
             }
         }
-        System.out.println("Начальная матрица b");
-        System.out.println("------");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.printf("%4d", b[i][j]);
-            }
-            System.out.println();
-        }
+        return matrix;
+    }
 
-        System.out.println();
-        System.out.println("Транспонированная матрица:");
+    public static void printMatrix(int x,int y, Matrix matrix) {
+        System.out.println("Матрица: ");
         System.out.println("------");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int transparentMatrix = new TransparentMatrix(a).getElement(j, i);
-                System.out.printf("%4d", transparentMatrix);
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println("Сложение матриц:");
-        System.out.println("------");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int sumOfTwoMatrices = new SumOfTwoMatrices(a,b).getElement(i, j);
-                System.out.printf("%4d", sumOfTwoMatrices);
+        int [][] a = new matrix;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.format("%s ", matrix.getElement(i, j));
             }
             System.out.println();
         }
     }
+
+    public static void main(String[] args) {
+        int i = 4;
+        int j = 4;
+
+        int[][] matrix1 = createMatrix1(i,j);
+        int[][] matrix2 = createMatrix2(i,j);
+
+        Matrix impl1 = new MatrixImpl(matrix1);
+        Matrix impl2 = new MatrixImpl(matrix2);
+
+        Matrix transpose = new TransparentMatrix(impl1);
+        Matrix sum = new SumOfTwoMatrices(impl1, impl2);
+
+        printMatrix(i, j, impl1);
+        printMatrix(i, j, impl2);
+        printMatrix(i, j, transpose);
+        printMatrix(i, j, sum);
+    }
+
 }
