6ae83e5a2bf4c8fa430f954ef5ee66db43e1e829
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-07-21 13:29:18 +0300

    Update SumOfTwoMatrices.java

--- HW_3/SumOfTwoMatrices.java
+++ HW_3/SumOfTwoMatrices.java
@@ -1,16 +1,16 @@
 package decorator;
 
 public class SumOfTwoMatrices implements Matrix {
-    int a[][];
-    int b[][];
+    private final Matrix a;
+    private final Matrix b;
 
-    public SumOfTwoMatrices(int[][] a, int[][] b) {
+    public SumOfTwoMatrices(Matrix a, Matrix b) {
         this.a = a;
         this.b = b;
     }
 
     @Override
     public int getElement(int i, int j) throws ArrayIndexOutOfBoundsException {
-        return a[i][j]+b[i][j];
+        return a.getElement(i,j)+b.getElement(i,j);
     }
 }
