e78d39a142935143880a0d494f5e816d23c5b1bc
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-07-21 13:30:08 +0300

    Update TransparentMatrixTest.java

--- HW_3/TransparentMatrixTest.java
+++ HW_3/TransparentMatrixTest.java
@@ -10,16 +10,18 @@ class TransparentMatrixTest {
     @Test
     void getElemenTransp() {
         int[][] a = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-        int currentResult = new TransparentMatrix(a).getElement(3,2);
-        Assert.assertEquals(12,currentResult);
+        MatrixImpl impl = new MatrixImpl(a);
+        int currentResult = new TransparentMatrix(impl).getElement(2,2);
+        Assert.assertEquals(9,currentResult);
 
     }
 
     @Test
     void getElemenTranspNext() {
         int[][] a = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-        int currentResult = new TransparentMatrix(a).getElement(0,2);
-        Assert.assertEquals(3,currentResult);
+        MatrixImpl impl = new MatrixImpl(a);
+        int currentResult = new TransparentMatrix(impl).getElement(0,2);
+        Assert.assertEquals(7,currentResult);
 
     }
 }
