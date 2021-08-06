e8b251bfe5854e4a4b748d41a1d2bd2ee7fd92ee
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-08-06 10:40:09 +0300

    Add files via upload

---
+++ zoo/model/Giraffe.java
@@ -0,0 +1,9 @@
+package zoo.model.speciesAnimals;
+
+import zoo.model.animal.Animal;
+
+public class Giraffe extends Animal {
+    public Giraffe(String name, Species species) {
+        super(name, species);
+    }
+}
